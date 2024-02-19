2a6dd8586d570535b99b5f44d9955f6790865655
Jakepps <nagalevsiy@mail.ru>
2024-02-19 21:24:20 +0300

    fix code

--- LR2/LR2.py
+++ LR2/LR2.py
@@ -1,25 +1,34 @@
 def legendre_symbol(a, p):
     """
-    Вычисляет символ Лежандра (a/p).
+    Вычисляет символ Лежандра a/p
     """
+    print("Вычисляем символ Лежандра для числа", a/p)
     ls = pow(a, (p - 1) // 2, p)
-    return ls if ls != p - 1 else -1
+
+    result = ls if ls != p - 1 else -1
+    print("Символ Лежандра для числа", a/p, "равен", result)
+
+    return result
 
 def quadratic_residues_or_non(p):
     """
-    Находит множество квадратичных вычетов по модулю p.
+    Находит множество квадратичных вычетов/невычетов по модулю p
     """
     residues = []
     non_residues = []
     for a in range(1, p):
+        print("------")
+        print("Проверяем цифру", a)
         if legendre_symbol(a, p) == 1:
             residues.append(a)
+            print("Цифра", a, "является квадратичным вычетом")
         else:
             non_residues.append(a)
+            print("Цифра", a, "является квадратичным невычетом")
     return residues, non_residues
-
 
 p = 7
 residues, non_residues = quadratic_residues_or_non(p)
+print("-------")
 print("Множество квадратичных вычетов по модулю", p, ":", residues)
 print("Множество квадратичных невычетов по модулю", p, ":", non_residues)
