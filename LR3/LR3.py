47ec875a2b1300691c04499d8f57e9004b02482a
Jakepps <nagalevsiy@mail.ru>
2024-03-05 00:03:18 +0300

    фиксы + объяснения

--- LR3/LR3.py
+++ LR3/LR3.py
@@ -4,16 +4,18 @@ class GaloisFieldCalculator:
         self.coeffs = coeffs
     
     def __add__(self, other):
+        #                 коэффициенты многочленов                             для определения максимальной степепени результирующего полинома
         result_coeffs = [(self.coeffs[i] + other.coeffs[i]) % 2 for i in range(max(len(self.coeffs), len(other.coeffs)))]
         return GaloisFieldCalculator(self.modulus_poly, result_coeffs)
 
     def __mul__(self, other):
         result_coeffs = [0] * (len(self.coeffs) + len(other.coeffs) - 1)
+        #вычисляем все возможные слагаемые в результирующем многочлене
         for i in range(len(self.coeffs)):
             for j in range(len(other.coeffs)):
                 result_coeffs[i+j] += self.coeffs[i] * other.coeffs[j]
         for i in range(len(result_coeffs)):
-            result_coeffs[i] %= 2
+            result_coeffs[i] %= 2 #нужно чтобы коэфф.оставались в поле GF(2)
         return GaloisFieldCalculator(self.modulus_poly, result_coeffs)
 
     def __sub__(self, other):
@@ -21,11 +23,13 @@ class GaloisFieldCalculator:
         return GaloisFieldCalculator(self.modulus_poly, result_coeffs)
 
     def __truediv__(self, other):
-        q = GaloisFieldCalculator(self.modulus_poly, [0])
-        r = self
-        while len(r.coeffs) >= len(other.coeffs):
+        q = GaloisFieldCalculator(self.modulus_poly, [0]) #частное
+        r = self #остаток
+        while len(r.coeffs) >= len(other.coeffs): #пока степень остатка не будет >= степени делителя
+            #определяем степени многочленов
             leading_degree = len(r.coeffs) - 1
             other_degree = len(other.coeffs) - 1
+            #выравниваем степени текущего остатка с делителем перед выполнением деления
             q += GaloisFieldCalculator(self.modulus_poly, [0]*(leading_degree - other_degree) + [1])
             r -= (GaloisFieldCalculator(self.modulus_poly, [0]*(leading_degree - other_degree) + [1]) * other)
         return q, r
@@ -45,6 +49,7 @@ class GaloisFieldCalculator:
     def multiplication_table(self):
         for i in range(1, len(self.coeffs)):
             for j in range(1, len(self.coeffs)):
+                #                                                 создает список из i+j-1 нулей, а [1] добавляет единицу в конец списка, что соответствует элементу поля с нужной степенью
                 result = GaloisFieldCalculator(self.modulus_poly, [0]*(i+j-1)+[1]) * GaloisFieldCalculator(self.modulus_poly, [0]*(i+j-1)+[1])
                 print(f"{i} * {j} = {result.coeffs}")
 
@@ -53,10 +58,12 @@ def input_GaloisFieldCalculator(modulus_poly):
     return GaloisFieldCalculator(modulus_poly, coeffs)
 
 def is_irreducible(poly):
-    # Проверяем деление на все многочлены меньшей степени
+    # перебираем все возможные делители степени многочлена poly, начиная с 1 и заканчивая половиной длины многочлена
     for i in range(1, len(poly) // 2 + 1):
         if len(poly) % i == 0:
+            # разделяем многочлен на части равной длины i
             factors = [poly[j:j+i] for j in range(0, len(poly), i)]
+            # проверяем, что все части, на которые был разделен многочлен, равны между собой
             if all(factor == factors[0] for factor in factors):
                 return False
     return True
