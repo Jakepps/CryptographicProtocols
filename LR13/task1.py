67d91d1279d8395b4b7a0d32392f175b185f5be4
Jakepps <nagalevsiy@mail.ru>
2024-03-17 14:21:17 +0300

    LR13 code

---
+++ LR13/task1.py
@@ -0,0 +1,39 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from os import urandom
+
+def encrypt_aes_ofb(key, iv, plaintext):
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv), backend=backend)
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_plaintext = padder.update(plaintext) + padder.finalize()
+    ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
+    return ciphertext
+
+def decrypt_aes_ofb(key, iv, ciphertext):
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv), backend=backend)
+    decryptor = cipher.decryptor()
+    decrypted_padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    plaintext = unpadder.update(decrypted_padded_plaintext) + unpadder.finalize()
+    return plaintext
+
+# Пример использования
+
+# Генерация ключа и вектора инициализации
+key = urandom(32)  # 256-bit ключ
+iv = urandom(16)   # 128-bit вектор инициализации
+
+# Текст, который нужно зашифровать
+plaintext = b"Hello, world!"
+
+# Шифрование
+ciphertext = encrypt_aes_ofb(key, iv, plaintext)
+print("Зашифрованный текст:", ciphertext)
+
+# Расшифрование
+decrypted_plaintext = decrypt_aes_ofb(key, iv, ciphertext)
+print("Расшифрованный текст:", decrypted_plaintext.decode())
